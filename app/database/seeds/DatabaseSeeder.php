24f00cd76722fb1881585f0c405623b5b88c2818
Ed Yarnold <ed@twixcoding.com>
2014-01-10 19:22:15 +0000

    Better player database seeding.

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -28,12 +28,25 @@ class PlayerTableSeeder extends Seeder {
 		// Create three example players
 		// We will use player1@ player2@ and player3@ footyracker.example.org as e-mail addresses.
 		// Because we're using soft delete, we want to make sure the factory doesnt automatically add these as deleted models.
-		Factory::create('\yegnold\footytracker\Player', array('first_name' => 'Abc', 'last_name' => 'Jkl', 'email' => 'player1@footytracker.example.org', 'deleted_at' => null));
-		Factory::create('\yegnold\footytracker\Player', array('first_name' => 'Def', 'last_name' => 'Mno', 'email' => 'player2@footytracker.example.org', 'deleted_at' => null));
-		Factory::create('\yegnold\footytracker\Player', array('first_name' => 'Ghi', 'last_name' => 'Pqr', 'email' => 'player3@footytracker.example.org', 'deleted_at' => null));
+		Factory::create('\yegnold\footytracker\Player', array('first_name' => 'Barry', 'last_name' => 'Y', 'email' => 'player1@footytracker.example.org', 'deleted_at' => null));
+		Factory::create('\yegnold\footytracker\Player', array('first_name' => 'Rich', 'last_name' => 'N', 'email' => 'player2@footytracker.example.org', 'deleted_at' => null));
+		Factory::create('\yegnold\footytracker\Player', array('first_name' => 'Russ', 'last_name' => 'X', 'email' => 'player3@footytracker.example.org', 'deleted_at' => null));
 
 		// Create an example player we can log in as...
 		Factory::create('\yegnold\footytracker\Player', array('first_name' => 'Edward', 'last_name' => 'Yarnold', 'email' => 'yegnold@gmail.com', 'password' => Hash::make('access'), 'deleted_at' => null));
+
+		Factory::create('\yegnold\footytracker\Player', array('first_name' => 'Will', 'last_name' => 'Y', 'email' => 'player4@footytracker.example.org', 'deleted_at' => null));
+		Factory::create('\yegnold\footytracker\Player', array('first_name' => 'Chris', 'last_name' => 'S', 'email' => 'player5@footytracker.example.org', 'deleted_at' => null));
+		Factory::create('\yegnold\footytracker\Player', array('first_name' => 'Big', 'last_name' => 'Tim', 'email' => 'player6@footytracker.example.org', 'deleted_at' => null));
+		Factory::create('\yegnold\footytracker\Player', array('first_name' => 'Little', 'last_name' => 'Tim', 'email' => 'player7@footytracker.example.org', 'deleted_at' => null));
+		Factory::create('\yegnold\footytracker\Player', array('first_name' => 'Policeman', 'last_name' => 'Dave', 'email' => 'player8@footytracker.example.org', 'deleted_at' => null));
+		Factory::create('\yegnold\footytracker\Player', array('first_name' => 'Hairy', 'last_name' => 'Dave', 'email' => 'player9@footytracker.example.org', 'deleted_at' => null));
+		Factory::create('\yegnold\footytracker\Player', array('first_name' => 'Alfie', 'last_name' => 'N', 'email' => 'player10@footytracker.example.org', 'deleted_at' => null));
+		Factory::create('\yegnold\footytracker\Player', array('first_name' => 'Matt', 'last_name' => 'W', 'email' => 'player11@footytracker.example.org', 'deleted_at' => null));
+		Factory::create('\yegnold\footytracker\Player', array('first_name' => 'Andy', 'last_name' => 'W', 'email' => 'player12@footytracker.example.org', 'deleted_at' => null));
+		Factory::create('\yegnold\footytracker\Player', array('first_name' => 'Little', 'last_name' => 'Will', 'email' => 'player13@footytracker.example.org', 'deleted_at' => null));
+		Factory::create('\yegnold\footytracker\Player', array('first_name' => 'Other', 'last_name' => 'Chris', 'email' => 'player14@footytracker.example.org', 'deleted_at' => null));
+
 	}
 
 }
